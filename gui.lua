4d23c7453ac5c85f3cf860bce6a20380cbaa6653
26ms <new_26ms+github.com@hotmail.com>
2024-11-06 10:17:31 +1100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,15 +1,13 @@
 local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
-local Window = UILib.new("Grand Piece Online", game.Players.LocalPlayer.UserId, "Buyer")
+local Window = UILib.new("Prestine Scripts", "dsc.gg/prestine", "Prestine's Scripts")
 local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
-local SubButton1 = Category1:Button("Combat", "http://www.roblox.com/asset/?id=8395747586")
+local SubButton1 = Category1:Button("Fishing", "http://www.roblox.com/asset/?id=8395747586")
 local Section1 = SubButton1:Section("Section", "Left")
-Section1:Toggle({
-    Title = "Teleport",
-    ButtonName = "Button",
-    Description = "Tp you to a saved location",
-    }, function(value)
-    print(value)
-end)
+local SubButton2 = Category1:Button("Misc", "http://www.roblox.com/asset/?id=8395747586")
+local Section2 = SubButton2:Section("Section", "Right")
+local Category2 = Window:Category("Super Misc", "http://www.roblox.com/asset/?id=8395621517")
+local SubButton3 = Category2:Button("Super Misc", "http://www.roblox.com/asset/?id=8395747586")
+local Section3 = SubButton3:Section("Section", "Right")
 Section1:Toggle({
     Title = "Auto Fish",
     Description = "Fully automatic fishing",
@@ -18,7 +16,7 @@ Section1:Toggle({
     print(value)
 end)
 Section1:Slider({
-    Title = "Delay",
+    Title = "Shake Delay",
     Description = "",
     Default = 0,
     Min = 0,
@@ -26,24 +24,51 @@ Section1:Slider({
     }, function(value)
     print(value)
 end)
-Section1:ColorPicker({
-    Title = "Gui Colour",
+Section1:Slider({
+    Title = "Fish Finish Delay",
     Description = "",
-    Default = Color3.new(255,0,0),
+    Default = 0,
+    Min = 0,
+    Max = 6
     }, function(value)
     print(value)
 end)
-Section1:Textbox({
-    Title = "Damage Multiplier",
+Section1:Slider({
+    Title = "Cast Delay",
     Description = "",
-    Default = "",
+    Default = 0,
+    Min = 0,
+    Max = 8
     }, function(value)
     print(value)
 end)
-Section1:Keybind({
-    Title = "Kill All",
+Section2:Slider({
+    Title = "Fishing Range",
     Description = "",
-    Default = Enum.KeyCode.Q,
+    Default = 10,
+    Min = 10,
+    Max = 100
     }, function(value)
     print(value)
-end)	
+    end)
+Section2:Slider({
+    Title = "Fishing Speed",
+    Description = "",
+    Default = 10,
+    Min = 1, 
+    Max = 20
+    }, function(value)
+    print(value)
+end)
+Section2:Toggle({
+    Title = "Fishing Auto-Cast",
+    Default = false,
+    }, function(value)
+    print(value)
+end)
+Section3:Toggle({
+    Title = "Fishing Auto-Cast",
+    Default = false,
+    }, function(value)
+    print(value)
+end)
