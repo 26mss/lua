a42ac3975085a68e7a93376c786100820c28c28a
26ms <new_26ms+github.com@hotmail.com>
2024-11-09 00:32:02 +1100

    Update gui.lua
    Changes to Fluent GUI library

--- gui.lua
+++ gui.lua
@@ -1,74 +1,93 @@
-local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
-local Window = UILib.new("Prestine Scripts", "dsc.gg/prestine", "Prestine's Scripts")
-local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
-local SubButton1 = Category1:Button("Fishing", "http://www.roblox.com/asset/?id=8395747586")
-local Section1 = SubButton1:Section("Section", "Left")
-local SubButton2 = Category1:Button("Misc", "http://www.roblox.com/asset/?id=8395747586")
-local Section2 = SubButton2:Section("Section", "Right")
-local Category2 = Window:Category("Super Misc", "http://www.roblox.com/asset/?id=8395621517")
-local SubButton3 = Category2:Button("Super Misc", "http://www.roblox.com/asset/?id=8395747586")
-local Section3 = SubButton3:Section("Section", "Right")
-Section1:Toggle({
-    Title = "Auto Fish",
-    Description = "Fully automatic fishing",
-    Default = false
-    }, function(value)
-    print(value)
-end)
-Section1:Slider({
-    Title = "Shake Delay",
-    Description = "",
-    Default = 0,
-    Min = 0,
-    Max = 4.1
-    }, function(value)
-    print(value)
-end)
-Section1:Slider({
-    Title = "Fish Finish Delay",
-    Description = "",
-    Default = 0,
-    Min = 0,
-    Max = 6
-    }, function(value)
-    print(value)
-end)
-Section1:Slider({
-    Title = "Cast Delay",
-    Description = "",
-    Default = 0,
-    Min = 0,
-    Max = 8
-    }, function(value)
-    print(value)
-end)
-Section2:Slider({
-    Title = "Fishing Range",
-    Description = "",
-    Default = 10,
-    Min = 10,
-    Max = 100
-    }, function(value)
-    print(value)
-    end)
-Section2:Slider({
-    Title = "Fishing Speed",
-    Description = "",
-    Default = 10,
-    Min = 1, 
-    Max = 20
-    }, function(value)
-    print(value)
-end)
-Section2:Toggle({
-    Title = "Fishing Auto-Cast",
-    Default = false,
-    }, function(value)
-    print(value)
-end)
-Section3:Toggle({
-    Title = "Fishing Auto-Cast",
-    Default = false,
-    }, function(value)
-    print(value)
-end)
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Prestine Key's ",
+    SubTitle = "by Ratet",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(880, 760),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Amethyst",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+--Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Key System", Icon = "key-round" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+
+local Options = Fluent.Options
+
+do
+
+    Tabs.Main:AddParagraph({
+        Title = "Prestine Key Checking System",
+        Content = "If your seeing this, you dont have a key, press the button below to copy the discord link to get your key"
+    })
+
+
+
+    Tabs.Main:AddButton({
+        Title = "Button",
+        Description = "Very important button",
+        Callback = function()
+            Window:Dialog({
+                Title = "",
+                Content = "This is a dialog",
+                Buttons = {
+                    {
+                        Title = "Confirm",
+                        Callback = function()
+                            print("Confirmed the dialog.")
+                        end
+                    },
+                    {
+                        Title = "Cancel",
+                        Callback = function()
+                            print("Cancelled the dialog.")
+                        end
+                    }
+                }
+            })
+        end
+    })
+
+
+-- Addons:
+-- SaveManager (Allows you to have a configuration system)
+-- InterfaceManager (Allows you to have a interface managment system)
+
+-- Hand the library over to our managers
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+-- Ignore keys that are used by ThemeManager.
+-- (we dont want configs to save themes, do we?)
+SaveManager:IgnoreThemeSettings()
+
+-- You can add indexes of elements the save manager should ignore
+SaveManager:SetIgnoreIndexes({})
+
+-- use case for doing it this way:
+-- a script hub could have themes in a global folder
+-- and game configs in a separate folder per game
+InterfaceManager:SetFolder("FluentScriptHub")
+SaveManager:SetFolder("FluentScriptHub/specific-game")
+
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+
+
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Fluent",
+    Content = "The script has been loaded.",
+    Duration = 8
+})
+
+-- You can use the SaveManager:LoadAutoloadConfig() to load a config
+-- which has been marked to be one that auto loads!
+SaveManager:LoadAutoloadConfig()
